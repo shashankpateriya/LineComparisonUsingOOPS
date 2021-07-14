ac46cb2f8ad73c50862cfb1568f6188ba73165c8
Shashank <shashankpateriya4@gmail.com>
2021-07-15 00:38:26 +0530

    [shashank] Add: Calculating Single line length

---
+++ UC1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisonOOPS
+{
+    class UC1
+    {
+        public static void SingleLine()
+        {
+            Console.WriteLine("Enter co-ordinates (x1, y1): ");
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter co-ordinates (x2, y2): ");
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double length = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+            Console.WriteLine("The Length of the line is: " + length);
+
+        }
+    }
+}
