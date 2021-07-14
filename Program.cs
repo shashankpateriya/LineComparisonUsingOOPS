f1dd77d4a8146abb77698b2df7cf1c3246db7db2
Shashank <shashankpateriya4@gmail.com>
2021-07-15 00:37:31 +0530

    [Shashank] Add: Main Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LineComparisonOOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //UC1.SingleLine();
+            //UC2.Equality();
+            UC3.CompareTo();
+        }
+    }
+}
